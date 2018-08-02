5dd9e4eb094f320afd98662b02070d56d733e818
akamemelord <shashank.yadav@daffodilsw.com>
2018-08-02 12:08:56 +0530

    code refactor with google oauth20

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+require('./services/passport');
 const app = express();
+require('./routes/authRoutes')(app); //for the routes
 
 
-app.get('/',(req,res)=>{
-	res.send({name:"GTA"});
-})
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+
+app.listen(PORT,()=>{
+	console.log("Server started");
+});
